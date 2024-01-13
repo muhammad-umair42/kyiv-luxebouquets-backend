d814646ceac32bcc395c7d87aa62069c5fca13b6
Muhammad Umair <umair42.dev@gmail.com>
2024-01-13 23:17:58 +0500

    finished

--- src/app.js
+++ src/app.js
@@ -2,24 +2,14 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 import multer from 'multer';
-import path, { dirname } from 'path';
-
-import { fileURLToPath } from 'url';
 const app = express();
-const origin = req.protocol + '://' + req.get('host');
+
 //app middlewares and configuration
-app.use(cors({ origin: origin, credentials: true }));
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-app.get('/', (req, res) => {
-  app.use(express.static(path.resolve(__dirname, 'client', 'dist')));
-  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
-});
 
 import categoryRouter from './routes/category.routes.js';
 import orderRouter from './routes/order.routes.js';
