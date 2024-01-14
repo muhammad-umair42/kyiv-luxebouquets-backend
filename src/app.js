5b142ae896b959f47a8bc33a599097a43f3e85f2
Muhammad Umair <umair42.dev@gmail.com>
2024-01-14 18:47:13 +0500

    build

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import multer from 'multer';
 const app = express();
 
 //app middlewares and configuration
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: `${process.env.CORS_ORIGIN}`, credentials: true }));
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
