933995b01bd87e725c3e8a60027ecb678b28b910
Abdul hameed <abdulhameed000650@gmail.com>
2024-08-19 15:34:44 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,9 +7,14 @@ import orderRouter from './routes/order.routes.js';
 import productRouter from './routes/product.routes.js';
 import userRouter from './routes/user.routes.js';
 const app = express();
-
+const allowedOrigins = [
+  'https://flower-shop-cleint.vercel.app',
+  'https://flower-shop-cleint-git-main-abdulhameed10s-projects.vercel.app',
+  'https://flower-shop-cleint-abdulhameed10s-projects.vercel.app',
+  'https://kyiv-luxebouquets-frontend-41h62vglq.vercel.app'
+];
 //app middlewares and configuration
-app.use(cors({ origin: `${process.env.CORS_ORIGIN}`, credentials: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json({ limit: '16kb' }));
 
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
