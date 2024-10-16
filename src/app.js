10c8eb7c35d6ac84c1a27512c5ce60e1ca17a23d
muhammad-umair42 <umair42.dev@gmail.com>
2024-10-17 00:00:51 +0500

    Update CORS configuration and allowed origins in app.js

--- src/app.js
+++ src/app.js
@@ -7,26 +7,22 @@ import orderRouter from './routes/order.routes.js';
 import productRouter from './routes/product.routes.js';
 import userRouter from './routes/user.routes.js';
 const app = express();
+// List of allowed origins
 const allowedOrigins = [
-  'https://kyiv-luxebouquets-frontend.vercel.app',
-  'http://localhost:5173',
-  'https://example.com',
+  'http://localhost:5173', // Your Vite app URL
+  'https://kyiv-luxebouquets-frontend.vercel.app', // Other allowed origins
 ];
 
+// CORS configuration
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      // Allow the request if the origin is in the allowed list or if no origin (server-side requests)
-      callback(null, true);
-    } else {
-      // Block the request if the origin is not in the allowed list
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: 'https://kyiv-luxebouquets-frontend.vercel.app', // Only allow requests from our Vite app
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true,
+  withCredentials: true, // Allow credentials (cookies, authorization headers, etc.)
 };
 
+// Apply CORS middleware
 app.use(cors(corsOptions));
 app.use(express.json({ limit: '16kb' }));
 
@@ -35,7 +31,7 @@ app.use(express.static('public'));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-  res.send('<h1></h1>');
+  res.send('<h1>Hello World</h1>');
 });
 
 app.use('/api/v1/users', userRouter);
